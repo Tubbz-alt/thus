49662ecf439c4001f0aa17b3f12c15e74480d09f
Ramon Buldó <rbuldo@gmail.com>
2016-03-01 17:35:28 +0100

    v0.9.4.2

--- thus/info.py
+++ thus/info.py
@@ -26,6 +26,6 @@
 
 """ Set some Thus global constants """
 
-THUS_VERSION = "0.9.4.1"
-THUS_LAST_STABLE = "0.9.4"
+THUS_VERSION = "0.9.4.2"
+THUS_LAST_STABLE = "0.9.4.1"
 THUS_WEBSITE = "http://www.manjaro.org"
